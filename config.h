ce64e414f32e987a652beabcda23e332afae7c0a
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-09-19 05:08:53 -0400

    feat: add scrollback patch

--- config.h
+++ config.h
@@ -234,6 +234,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
--- st.h
+++ st.h
@@ -83,6 +83,8 @@ void redraw(void);
 void draw(void);
 
 void newterm(const Arg *);
+void kscrolldown(const Arg *);
+void kscrollup(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
