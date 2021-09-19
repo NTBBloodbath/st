423729021969728f50065eaf842b90862a3ccdf4
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-09-19 06:58:52 -0400

    chore: reduce icons font size

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@
 static char *font = "Iosevka Slab:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true"
+    "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true"
 };
 
 static int borderpx = 0;
